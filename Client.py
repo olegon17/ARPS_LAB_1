d51ba6a47e802d03969511428186f26ba27cd9dd
oleg <oleg-1798@mail.ru>
2020-03-03 00:04:45 +0500

    1

--- Client.py
+++ Client.py
@@ -3,9 +3,6 @@
 import socket
 from tkinter import *
 
-
-
-
 tk=Tk()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,20 +12,30 @@ s.bind(('0.0.0.0',11719))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+#if (sock.done)
 
 text=StringVar()
 name=StringVar()
-name.set('HabrUser')
+name.set('ВВЕДИ ИМЯ')
 text.set('')
 tk.title('MegaChat')
-tk.geometry('400x300')
+tk.geometry('666x666')
 
 log = Text(tk)
-nick = Entry(tk, textvariable=name)
+nickname = Entry(tk, textvariable=name)
 msg = Entry(tk, textvariable=text)
 msg.pack(side='bottom', fill='x', expand='true')
-nick.pack(side='bottom', fill='x', expand='true')
+nickname.pack(side='bottom', fill='x', expand='true')
 log.pack(side='top', fill='both',expand='true')
+
+def close_window():
+	msg_text=name.get() + " вышел из чата"
+	sock.sendto (msg_text.encode(),('255.255.255.255',11719))
+	text.set('')
+	sock.close()
+	tk.close()
+
+tk.protocol("WM_DELETE_WINDOW", close_window)
 
 def loopproc():
 	log.see(END)
