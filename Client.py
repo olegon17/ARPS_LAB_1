94007647b3515ac5f4cc3292a77b192ed4b0c3f3
olegp <oleg-1798@mail.ru>
2020-03-02 13:55:53 +0500

    konez 2 varianta

--- Client.py
+++ Client.py
@@ -1,74 +1,53 @@
-import socket
+	# telnet program example
+import socket, select, string, sys
 
-PORT = 9070;
-HOST = "localhost";
-userName = "noName";
+def prompt() :
+	sys.stdout.write('<You> ')
+	sys.stdout.flush()
 
-import struct
-
-class DataInputStream:
-	def __init__(self, stream):
-        self.stream = stream
-
-    def read_boolean(self):
-        return struct.unpack('?', self.stream.read(1))[0]
-
-    def read_byte(self):
-        return struct.unpack('b', self.stream.read(1))[0]
-
-    def read_unsigned_byte(self):
-        return struct.unpack('B', self.stream.read(1))[0]
-
-    def read_char(self):
-        return chr(struct.unpack('>H', self.stream.read(2))[0])
-
-    def read_double(self):
-        return struct.unpack('>d', self.stream.read(8))[0]
-
-    def read_float(self):
-        return struct.unpack('>f', self.stream.read(4))[0]
-
-    def read_short(self):
-        return struct.unpack('>h', self.stream.read(2))[0]
-
-    def read_unsigned_short(self):
-        return struct.unpack('>H', self.stream.read(2))[0]
-
-    def read_long(self):
-        return struct.unpack('>q', self.stream.read(8))[0]
-
-    def read_utf(self):
-        utf_length = struct.unpack('>H', self.stream.read(2))[0]
-        return self.stream.read(utf_length)
-
-    def read_int(self):
-        return struct.unpack('>i', self.stream.read(4))[0]
-
-sock = socket.socket()
-print("Hello, type your name \n");
-input(userName)
-
-
-try(sock.connect((userName, 9070)))
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
-    DataInputStream ois = new DataInputStream(socket.getInputStream());){
-
-
-
-
-
-
-
-
-
-
-
-
-sock.send(b'Hello!\n')
-
-data = sock.recv(1024)
-udata = data.decode("utf-8")
-print("Server > " + udata)
-
-sock.close()
+#main function
+if __name__ == "__main__":
+	
+	if(len(sys.argv) < 3) :
+		print ('Usage : python telnet.py hostname port')
+		sys.exit()
+	
+	host = sys.argv[1]
+	port = int(sys.argv[2])
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(2)
+	
+	# connect to remote host
+	try :
+		s.connect((host, port))
+	except :
+		print ('Unable to connect')
+		sys.exit()
+	
+	print ('Connected to remote host. Start sending messages')
+	prompt()
+	
+	while 1:
+		socket_list = [sys.stdin, s]
+		
+		# Get the list sockets which are readable
+		read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+		
+		for sock in read_sockets:
+			#incoming message from remote server
+			if sock == s:
+				data = sock.recv(4096)
+				if not data :
+					print ('\nDisconnected from chat server')
+					sys.exit()
+				else :
+					#print data
+					sys.stdout.write(data)
+					prompt()
+			
+			#user entered a message
+			else :
+				msg = sys.stdin.readline()
+				s.send(msg.encode())
+				prompt()
